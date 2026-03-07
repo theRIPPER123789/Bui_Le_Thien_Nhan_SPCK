ae27d078f0a4883e9326163e182f1cd10498b32f
BuiLeThienNhan <popcatgaming240@gmail.com>
2026-03-07 13:53:41 +0700

    Updater func: login and register add validate

--- js/register.js
+++ js/register.js
@@ -12,6 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
 
+        let lowerCaseLetter = /[a-z]/g;
+        let upperCaseLetter = /[A-Z]/g;
+        let number = /[0-9]/g;
+
         if (!username || !fullname || !phone || !email || !password) {
             alert("Vui lòng nhập đầy đủ thông tin!");
             return;
@@ -25,13 +29,36 @@ document.addEventListener("DOMContentLoaded", function () {
             password: password
         };
 
+        if (password.length < 6) {
+            alert("Mật khẩu phải lớn hơn 6 chữ số");
+            return;
+        }
+
+        if (!password.match(upperCaseLetter)) {
+            alert("Mật khẩu cần ít nhất một chữ cái in hoa!");
+            return;
+        }
+
+        if (!password.match(lowerCaseLetter)) {
+            alert("Mật khẩu cần ít nhất một chữ thường!");
+            return;
+        }
         // lưu vào localStorage
         localStorage.setItem("user", JSON.stringify(user));
 
-        alert("Đăng ký thành công!");
-
-        // chuyển sang trang login
-        window.location.href = "login.html";
+        //Kiểm tra email đã được đăng ký chưa
+        if (user[email]) {
+            alert("Email này đã được dăng ký!");
+            return;
+        } else {
+            user[email] = {
+                email: email,
+                password: password
+            };
+            localStorage.setItem("user", JSON.stringify(user));
+            alert("Đăng ký thành công!");
+            window.location.href = "login.html";//Chuyển hướng đến trang đăng nhập
+        }
 
     });
 
